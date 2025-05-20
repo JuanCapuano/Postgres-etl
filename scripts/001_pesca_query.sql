948ce109373d3abaedbd3cc260778d51dd32cbae
Santiago Falvo <falvosanti@gmail.com>
2025-05-20 11:43:08 -0300

    Cambio de consultas

--- scripts/001_pesca_query.sql
+++ scripts/001_pesca_query.sql
@@ -13,30 +13,28 @@ JOIN public.provincia p ON d.provincia_id = p.id
 GROUP BY p.nombre
 ORDER BY total_captura DESC;
 
--- Ejemplo: Top 3 de especies más capturadas por provincia y departamento
+-- Ejemplo: Top 3 de especies más capturadas a nivel nacional
 
 SELECT
-  p.nombre AS provincia,
-  d.nombre AS departamento,
   pe.especie,
   SUM(pe.captura) AS total_captura
 FROM public.pesca pe
-JOIN public.departamento d ON pe.departamento_id = d.id
-JOIN public.provincia p ON d.provincia_id = p.id
-GROUP BY p.nombre, d.nombre, pe.especie
+GROUP BY pe.especie
 ORDER BY total_captura DESC
 LIMIT 3;
 
 
--- Ejemplo: Especie más capturada por provincia
+-- Ejemplo: Cantidad de especies distintas capturadas 
+-- por provincia y departamento
 
 SELECT
   p.nombre AS provincia,
-  pe.especie,
-  SUM(pe.captura) AS total_captura
-FROM public.pesca pe
-JOIN public.departamento d ON pe.departamento_id = d.id
-JOIN public.provincia p ON d.provincia_id = p.id
-GROUP BY p.nombre, pe.especie
-ORDER BY total_captura DESC
-LIMIT 1;
+  d.nombre AS departamento,
+  COUNT(DISTINCT pe.especie) AS cantidad_especies
+FROM pesca pe
+JOIN departamento d ON pe.departamento_id = d.id
+JOIN provincia p ON d.provincia_id = p.id
+GROUP BY p.nombre, d.nombre
+ORDER BY cantidad_especies DESC;
+
+
